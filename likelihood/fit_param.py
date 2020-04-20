581265bc581a6fd727310fd7de6c4870f572a140
Michael J. Harms <harmsm@gmail.com>
2020-04-20 14:28:42 -0700

    added fit parameter class and tests thereof

---
+++ likelihood/fit_param.py
@@ -0,0 +1,375 @@
+__description__ = \
+"""
+Main class for holding fit parameters, including guesses, values, ranges, etc.
+"""
+__date__ = "2016-09-02"
+__author__ = "Michael J. Harms"
+
+import copy
+import numpy as np
+
+import warnings
+
+_INFINITY_PROXY = 1e9
+_CLOSE_TO_ZERO = 1e6
+
+class FitParameter:
+    """
+    Class for storing and manipulating fit parameters.
+
+    Fit control attributes:
+    -----------------------
+
+        + .guess: (float) guess value for fit
+        + .fixed: (bool) whether to float parameter or not
+        + .bounds: [float,float] lower and upper bounds for parameter
+        + .alias: (hashable) name of global parameter.  if not linked to global
+                  parameter, will be None.
+
+    These attributes can be set on __init__ or directly via instance.xxx = value.
+    If any of these values are set **loaded fit results are wiped out** to avoid
+    inconsistency between the fit control parameters and the resulting fit.
+
+    Fit result attributes:
+    ----------------------
+
+        + .value: (float) value of parameter estimated by the fit.
+                  Before the fit is run, this will be the .guess value.
+        + .stdev: (float) standard deviation on the estimate of the fit parameter.
+                  Before the fit is run, this will be None.
+        + .ninetyfive: (float,float) top and bottom of the 95% confidence interval.
+                  Before the fit is run, this will be None.
+        + .is_fit_result: (bool) whether or not the parameter contains the results
+                          of a fit.
+
+    These attributes can only be updated using the load_fit_result method, which
+    takes a fitter instance as an argument.
+
+    Metadata attributes:
+    --------------------
+
+        + .name: (str) name of fit parameter
+
+    This attribute can be set on __init__ or directly via instance.name = value.
+    The value is used for convenience only and is ignored by other methods.
+    """
+
+    def __init__(self,name,guess=None,fixed=False,bounds=None,alias=None):
+        """
+        Initialize class.  Parameters:
+
+        name: name of parameter (string)
+        guess: parameter guess (float).
+               If None:
+                    + If bounds are given:
+                        - If lower and upper bounds have the same sign, the guess
+                          will be placed at the geometric mean of the two bounds.
+                        - Otherwise, the guess is placed at the arithmetic mean
+                          of the two bounds.
+                    + If bounds are not given, guess will be set to 1.0.
+        fixed: whether or not the parameter is fixed (bool)
+        bounds: bounds on fit for parameter (list-like object of 2 floats). If
+                None, bounds will be set to (None,None).  If (None,5), no lower
+                bound, upper bound of 5.
+        alias: alias for parameter name, for linking to global paramter names. (str)
+               If None, no alias is made.
+        """
+
+        # Setting must be in this order. If no guess is specified, the guess
+        # is made based on the bounds.
+        self._is_fit_result = False
+        self.bounds = bounds
+        self.guess = guess
+
+        # Simple terms
+        self.name = name
+        self.fixed = fixed
+        self.alias = alias
+
+
+    #--------------------------------------------------------------------------
+    # parameter name
+
+    @property
+    def name(self):
+        """
+        Name of the parameter.
+        """
+
+        try:
+            return self._name
+        except AttributeError:
+            return None
+
+    @name.setter
+    def name(self,name):
+        self._name = str(name)
+
+
+    #--------------------------------------------------------------------------
+    # parameter guess
+
+    @property
+    def guess(self):
+        """
+        Guess for the parameter.
+        """
+
+        try:
+            return self._guess
+        except AttributeError:
+            return None
+
+    @guess.setter
+    def guess(self,guess):
+        f"""
+        Set the guess.  If None and no bounds are specified, set to 1.0.  If
+        None and bounds are specified, set to geometric mean of the bounds.
+        If one of the bounds is infinte in this latter case, set that bound to
+        {_INFINITY_PROXY} for the purpose of the midpoint calculation.
+        """
+
+        if guess is not None:
+
+            try:
+                guess = np.float(guess)
+            except (ValueError,TypeError):
+                err = f"parameter guess '{guess}' cannot be interpretable as a float\n"
+                raise ValueError(err)
+
+        else:
+
+            if self.bounds[0] == -np.inf and self.bounds[1] == np.inf:
+                guess = 1.0
+            else:
+
+                # If we have non-infinite bounds, take the geometric midpoint of
+                # the bounds as the guess
+                if self.bounds[0] > -np.inf:
+                    lower_bound = self.bounds[0]
+                    lower_sign = np.abs(lower_bound)/lower_bound
+                else:
+                    lower_bound = -_INFINITY_PROXY
+                    lower_sign = -1
+
+                if self.bounds[1] < np.inf:
+                    upper_bound = self.bounds[1]
+                    upper_sign = np.abs(upper_bound)/upper_bound
+                else:
+                    upper_bound = _INFINITY_PROXY
+                    upper_sign = 1
+
+                # If the bounds have the same sign, use geometric mean
+                if upper_sign == lower_sign:
+                    l = np.log(np.abs(lower_bound))
+                    u = np.log(np.abs(upper_bound))
+                    guess = upper_sign * np.exp((u + l)/2)
+
+
+                # If they have opposite signs, use the arithmetic mean
+                elif upper_sign > lower_sign:
+                    guess = (upper_bound + lower_bound)/2
+
+                # This should *never* be true. This would imply the upper
+                # bound is negative and the lower bound is positive.
+                else:
+                    err = f"Could not set guess automatically: lower bound \n"
+                    err += f"({self.bounds[0]}) somehow above upper bound ({self.bound[1]}).\n"
+                    err += "this is probably a bug in the likelihood code. You\n"
+                    err += "can file a bug report at https://github.com/harmslab/likelihood\n"
+                    raise RuntimeError(err)
+
+        # Make sure the guess is within bounds
+        if guess > self.bounds[1] or guess < self.bounds[0]:
+            err = f"guess ({guess}) outside bounds ({self.bounds[0],self.bounds[1]})\n"
+            raise ValueError(err)
+
+        self._guess = guess
+
+        self._clear_fit_result()
+
+    #--------------------------------------------------------------------------
+    # parameter fixed-ness.
+
+    @property
+    def fixed(self):
+        """
+        Whether or not the parameter if fixed.
+        """
+
+        return self._fixed
+
+    @fixed.setter
+    def fixed(self,bool_value):
+        """
+        Fix or unfix the parameter.
+        """
+
+        self._fixed = bool(bool_value)
+
+        self._clear_fit_result()
+
+    #--------------------------------------------------------------------------
+    # bounds for fit.
+
+    @property
+    def bounds(self):
+        """
+        Fit bounds.  Either list of bounds or None.
+        """
+
+        try:
+            return self._bounds
+        except AttributeError:
+            return None
+
+    @bounds.setter
+    def bounds(self,bounds):
+        """
+        Set fit bounds.
+        """
+
+        if bounds is not None:
+
+            try:
+                if len(bounds) != 2:
+                    raise TypeError
+
+                bounds = np.array(bounds,dtype=np.float)
+
+            except TypeError:
+                err = "Bounds must be list-like object of length 2\n"
+                raise ValueError(err)
+
+        else:
+            bounds = np.array((-np.inf,np.inf))
+
+        # Set bounds very close to zero to zero.
+        equiv_to_zero = np.finfo(bounds.dtype).tiny*_CLOSE_TO_ZERO
+        bounds[np.abs(bounds) < equiv_to_zero] = 0.0
+
+        # Make sure upper bound is above the lower bound
+        if bounds[1] <= bounds[0]:
+            err = f"upper bound ({bounds[1]}) must be greater than lower bound ({bounds[0]})"
+            raise ValueError(err)
+
+        if self.guess is not None:
+            if self.guess < bounds[0]:
+                old_guess = self.guess
+                self.guess = bounds[0]
+            elif self.guess > bounds[1]:
+                old_guess = self.guess
+                self.guess = bounds[1]
+            else:
+                old_guess = None
+
+            if old_guess is not None:
+                w = f"The previous guess ({old_guess}) is outside the new bounds ({bounds})\n"
+                w += f"Guess has been updated to {self.guess}\n"
+                warnings.warn(w,RuntimeWarning)
+
+        self._bounds = bounds
+        self._clear_fit_result()
+
+    #--------------------------------------------------------------------------
+    # parameter alias
+
+    @property
+    def alias(self):
+        """
+        Parameter alias.  Either string or None.
+        """
+
+        try:
+            return self._alias
+        except AttributeError:
+            return None
+
+    @alias.setter
+    def alias(self,alias):
+        """
+        Set alias.
+        """
+
+        # Will throw an error if alias is not hashable
+        try:
+            {alias:None}
+        except TypeError:
+            err = f"alias ({alias}) is not hashable.\n"
+            raise ValueError(err)
+
+        self._alias = copy.deepcopy(alias)
+        self._clear_fit_result()
+
+    #--------------------------------------------------------------------------
+    # Properties that are set by the fitter, but not the user.
+
+    @property
+    def value(self):
+        """
+        Value of the parameter.
+        """
+
+        try:
+            return self._value
+        except AttributeError:
+            return None
+
+
+    @property
+    def stdev(self):
+        """
+        Standard deviation on the parameter.
+        """
+
+        try:
+            return self._stdev
+        except AttributeError:
+            return None
+
+    @property
+    def ninetyfive(self):
+        """
+        95% confidence interval on the parameter.
+        """
+
+        try:
+            return self._ninetyfive
+        except AttributeError:
+            return None
+
+    @property
+    def is_fit_result(self):
+        """
+        Whether the value shown is the result of a fit.
+        """
+
+        return self._is_fit_result
+
+
+    def load_fit_result(self,fitter,param_number):
+        """
+        Update standard deviation, ninetyfive, parameter value, from a successful
+        fit.
+
+        fitter: Fitter instance.
+        param_number: number corresponding to this parameter in the fit parameter
+                      vector.
+        """
+
+        if fitter.success:
+            self._value = fitter.estimate[param_number]
+            self._stdev = fitter.stdev[param_number]
+            self._ninetyfive = fitter.ninetyfive[param_number]
+            self._is_fit_result = True
+
+    def _clear_fit_result(self):
+        """
+        Cleared the fit result.  Called when attributes controlling the fit
+        are modified to avoid inconsistency.
+        """
+
+        self._value = self.guess
+        self._stdev = None
+        self._ninetyfive = None
+        self._is_fit_result = False
---
+++ tests/test_fit_param.py
@@ -0,0 +1,300 @@
+import pytest
+
+import likelihood
+
+import numpy as np
+
+def test_init():
+
+    with pytest.raises(TypeError):
+        p = likelihood.FitParameter()
+
+    p = likelihood.FitParameter(name="test")
+
+    assert np.array_equal(p.bounds,np.array((-np.inf,np.inf)))
+    assert p.guess == 1.0
+    assert p.value == p.guess
+    assert p.fixed == False
+    assert p.alias is None
+
+
+def test_name_getter_setter():
+
+    # Default (must be set via __init__)
+    p = likelihood.FitParameter(name="test")
+    assert p.name == "test"
+
+    # Set directly
+    p.name = 5
+    assert p.name == "5"
+
+    # Set directly
+    p.name = "junk"
+    assert p.name == "junk"
+
+
+def test_guess_getter_setter():
+
+    # Default
+    p = likelihood.FitParameter(name="test")
+    assert np.array_equal(p.bounds,np.array((-np.inf,np.inf)))
+    assert p.guess == 1.0
+
+    # Set via __init__
+    p = likelihood.FitParameter(name="test",guess=12)
+    assert p.guess == 12
+
+    # Set directly
+    p = likelihood.FitParameter(name="test")
+    p.guess = 22
+    assert p.guess == 22
+
+    # --- bad value checks ---
+    p = likelihood.FitParameter(name="test")
+
+    with pytest.raises(ValueError):
+        p.guess = "test"
+
+    with pytest.raises(ValueError):
+        p.guess = [1.0]
+
+    with pytest.raises(ValueError):
+        p.guess = np.arange(10)
+
+    # --- bound check ---
+
+    with pytest.raises(ValueError):
+        p = likelihood.FitParameter(name="test")
+        p.bounds = [-10,10]
+        p.guess = -20
+
+    with pytest.raises(ValueError):
+        p = likelihood.FitParameter(name="test")
+        p.bounds = [-10,10]
+        p.guess = 20
+
+def test_bounds_getter_setter():
+
+    # Default
+    p = likelihood.FitParameter(name="test")
+    assert np.array_equal(p.bounds,np.array((-np.inf,np.inf)))
+
+    # Set via __init__
+    bounds = [1,2]
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    assert np.array_equal(p.bounds,np.array(bounds))
+
+    # Set directly
+    bounds = [1,2]
+    p.bounds = bounds
+    assert np.array_equal(p.bounds,np.array(bounds))
+
+    # --- bad value checks ---
+    with pytest.raises(ValueError):
+        p.bounds = "test"
+
+    with pytest.raises(ValueError):
+        p.bounds = "te"
+
+    with pytest.raises(ValueError):
+        p.bounds = 1.0
+
+    with pytest.raises(ValueError):
+        p.bounds = [1.0]
+
+    with pytest.raises(ValueError):
+        p.bounds = [1.0,1.0]
+
+    with pytest.raises(ValueError):
+        p.bounds = [1.0,-1.0]
+
+    with pytest.raises(ValueError):
+        p.bounds = ["a","b"]
+
+def test_interaction_bounds_guesses():
+
+    # --- default guess depends on bounds; try different bounds scenarios ---
+
+    # Two positive bounds
+    bounds = [10,20]
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert np.isclose(p.guess,np.exp(np.mean(np.log(bounds))))
+
+    # Two negative bounds
+    bounds = [-20,-10]
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert np.isclose(p.guess,-np.exp(np.mean(np.log(np.abs(bounds)))))
+
+    # One negative, one positive
+    bounds = [-10,10]
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert np.isclose(p.guess,np.sum(bounds)/2)
+
+    # negative infinity, positive real
+    bounds = [-np.inf,10]
+    internal_bounds = [-likelihood.fit_param._INFINITY_PROXY,bounds[1]]
+    expected = np.sum(internal_bounds)/2
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert np.isclose(p.guess,expected)
+
+    # negative real, positive infinity
+    bounds = [-10,np.inf]
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    internal_bounds = [bounds[0],likelihood.fit_param._INFINITY_PROXY]
+    expected = np.sum(internal_bounds)/2
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert np.isclose(p.guess,expected)
+
+    # negative infinity, positive infinity
+    bounds = [-np.inf,np.inf]
+    p = likelihood.FitParameter(name="test",bounds=bounds)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert p.guess == 1.0
+
+    # --- Update bounds such that guess is outside the new bounds ---
+
+    bounds = [-10,10]
+    p = likelihood.FitParameter(name="test",bounds=bounds,guess=-5)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert p.guess == -5.0
+
+    new_bounds = [0,10]
+    with pytest.warns(RuntimeWarning):
+        p.bounds = new_bounds
+    assert np.array_equal(p.bounds,np.array(new_bounds))
+    assert p.guess == new_bounds[0]
+
+    bounds = [-10,10]
+    p = likelihood.FitParameter(name="test",bounds=bounds,guess=5)
+    assert np.array_equal(p.bounds,np.array(bounds))
+    assert p.guess == 5.0
+
+    new_bounds = [-10,0]
+    with pytest.warns(RuntimeWarning):
+        p.bounds = new_bounds
+    assert np.array_equal(p.bounds,np.array(new_bounds))
+    assert p.guess == new_bounds[1]
+
+
+def test_alias_setter_getter():
+
+    # Default
+    p = likelihood.FitParameter(name="test")
+    assert p.alias is None
+
+    # Set via __init__
+    p = likelihood.FitParameter(name="test",alias="some_alias")
+    assert p.alias is "some_alias"
+
+    # Set directly
+    p = likelihood.FitParameter(name="test")
+    assert p.alias is None
+    p.alias = "alias!"
+    assert p.alias is "alias!"
+    p.alias = 22
+    assert p.alias is 22
+
+    # --- bad value checks ---
+    p = likelihood.FitParameter(name="test")
+
+    with pytest.raises(ValueError):
+        p.alias = [1,3]
+
+    with pytest.raises(ValueError):
+        p.alias = np.array([1,23])
+
+def test_load_clear_fit_results(fitter_object):
+
+    # --- Make sure we can load fit result into parameter ---
+    p = likelihood.FitParameter(name="test")
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert p.alias is None
+    assert not p.is_fit_result
+
+    p.load_fit_result(fitter_object,0)
+    assert p.value == fitter_object.estimate[0]
+    assert p.stdev == fitter_object.stdev[0]
+    assert np.array_equal(p.ninetyfive,fitter_object.ninetyfive[0])
+    assert p.alias is None
+    assert p.is_fit_result
+
+    # --- Make sure setting guess wipes out fit ---
+    p.guess = 22
+    assert p.guess == 22
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert not p.is_fit_result
+
+    # --- Make sure setting alias wipes out fit ---
+
+    p = likelihood.FitParameter(name="test")
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert p.alias is None
+    assert not p.is_fit_result
+
+    p.load_fit_result(fitter_object,0)
+    assert p.value == fitter_object.estimate[0]
+    assert p.stdev == fitter_object.stdev[0]
+    assert np.array_equal(p.ninetyfive,fitter_object.ninetyfive[0])
+    assert p.is_fit_result
+
+    p.alias = "alias!"
+    assert p.alias == "alias!"
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert not p.is_fit_result
+
+    # --- Make sure setting bounds wipes out fit ---
+
+    p = likelihood.FitParameter(name="test")
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert np.array_equal(p.bounds,np.array((-np.inf,np.inf)))
+    assert not p.is_fit_result
+
+    p.load_fit_result(fitter_object,0)
+    assert p.value == fitter_object.estimate[0]
+    assert p.stdev == fitter_object.stdev[0]
+    assert np.array_equal(p.ninetyfive,fitter_object.ninetyfive[0])
+    assert p.is_fit_result
+
+    p.bounds = [-100,100]
+    assert np.array_equal(p.bounds,np.array((-100,100)))
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert not p.is_fit_result
+
+
+    # --- Make sure setting fixed wipes out fit ---
+
+    p = likelihood.FitParameter(name="test")
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert p.fixed == False
+    assert not p.is_fit_result
+
+    p.load_fit_result(fitter_object,0)
+    assert p.value == fitter_object.estimate[0]
+    assert p.stdev == fitter_object.stdev[0]
+    assert np.array_equal(p.ninetyfive,fitter_object.ninetyfive[0])
+    assert p.is_fit_result
+
+    p.fixed = True
+    assert p.fixed == True
+    assert p.value == p.guess
+    assert p.stdev is None
+    assert p.ninetyfive is None
+    assert not p.is_fit_result
